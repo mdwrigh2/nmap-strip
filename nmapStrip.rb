478108892cf8071ed3bfa57b412d6038c6f2f1e1
Michael Wright <mdwrigh2@ncsu.edu>
2009-11-09 10:44:31 -0500

    adding the script

---
+++ nmapStrip.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+if !ARGV[0]
+  puts"   NAME"
+  puts "nmapStrip.rb [infile] [outfile] \n\n"
+  puts "  SUMMARY"
+  puts "A quick script to strip nmap scans of down hosts, thus showing you only good ones. \n\n"
+  puts "  ARGUMENTS"
+  puts "[infile] - The output of the nmap scan you want to strip"
+  puts "[outfile] - Where you want to write the new, stripped, file to\n\n"
+  exit(1)
+end
+
+begin
+  file = File.new(ARGV[0], "r")
+  outfile = File.new(ARGV[1], "a")
+  while (line = file.gets)
+    if (line =~ /appears to be down./) == nil
+      outfile.write(line)
+    end
+  end
+  file.close
+  outfile.close
+rescue Exception => e
+  puts "err: #{e}"
+end
+
