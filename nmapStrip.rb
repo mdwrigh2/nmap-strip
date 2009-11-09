727cae0239a9a18a7d5397538ff0f3513eb7c6a2
Michael Wright <mdwrigh2@ncsu.edu>
2009-11-09 11:45:26 -0500

    Fixed it so the help script will appear when only given one argument

--- nmapStrip.rb
+++ nmapStrip.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
-if !ARGV[0]
+if !ARGV[1] 
   puts"   NAME"
   puts "nmapStrip.rb [infile] [outfile] \n\n"
   puts "  SUMMARY"
   puts "A quick script to strip nmap scans of down hosts, thus showing you only good ones. \n\n"
   puts "  ARGUMENTS"
-  puts "[infile] - The output of the nmap scan you want to strip"
-  puts "[outfile] - Where you want to write the new, stripped, file to\n\n"
+  puts "[infile] - The output of the nmap scan you want to strip."
+  puts "[outfile] - Where you want to write the new, stripped, file to.\n\n"
   exit(1)
 end
 
